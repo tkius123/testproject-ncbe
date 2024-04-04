6d4c3f9ab94e8f6aada7e7805f7fda2faa289e41
Tim Lee <artemis930101>
2024-04-04 06:30:06 -0400

    add WorkLog

--- src/main/java/com/example/testprojectncbe/repository/WorkLogRepository.java
+++ src/main/java/com/example/testprojectncbe/repository/WorkLogRepository.java
@@ -3,5 +3,8 @@ package com.example.testprojectncbe.repository;
 import com.example.testprojectncbe.model.WorkLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkLogRepository extends JpaRepository<WorkLog, Long> {
+    List<WorkLog> findByWeekId(Long weekId);
 }
