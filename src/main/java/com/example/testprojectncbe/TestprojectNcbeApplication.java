ebd5de2d4f939d197d6783bd850e56a589e7a251
Tim Lee <artemis930101>
2024-04-04 08:57:44 -0400

    add swagger

--- src/main/java/com/example/testprojectncbe/TestprojectNcbeApplication.java
+++ src/main/java/com/example/testprojectncbe/TestprojectNcbeApplication.java
@@ -1,8 +1,11 @@
 package com.example.testprojectncbe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Codechallenge", version = "v1", description = "Documentation of Codechallenge"))
 @SpringBootApplication
 public class TestprojectNcbeApplication {
 
